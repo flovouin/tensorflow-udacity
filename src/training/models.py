3376beb28e10e8f6a396091889a89c7aa1f6b04a
Flo <florent.vouin@gmail.com>
2016-02-09 14:54:13 +0100

    Add LSTM to training models.

--- src/training/models.py
+++ src/training/models.py
@@ -187,3 +187,112 @@ def cbow_model(vocabulary_size, embedding_size, context_length, batch_size,
         'labels_ph': tf_train_labels }
 
     return tf_graph, optimizer, loss, normalized_embeddings, similarity
+
+# LSTM.
+def lstm_model(input_size, output_size, embedding, num_nodes, num_unrollings, batch_size,
+    learning_rate, exp_decay = None, gradient_max_value = 1.25, dropout_prob = 0.0):
+
+    graph = tf.Graph()
+    with graph.as_default():
+        # [ix, fx, cx, ox]
+        x_mat = tf.Variable(tf.truncated_normal([input_size, 4*num_nodes], -0.1, 0.1))
+        # [im, fm, cm, om]
+        o_mat = tf.Variable(tf.truncated_normal([num_nodes, 4*num_nodes], -0.1, 0.1))
+        # [ib, fb, cb, ob]
+        b_vec = tf.Variable(tf.zeros([1, 4*num_nodes]))
+
+        # Variables saving state across unrollings.
+        saved_output = tf.Variable(tf.zeros([batch_size, num_nodes]), trainable=False)
+        saved_state = tf.Variable(tf.zeros([batch_size, num_nodes]), trainable=False)
+        # Classifier weights and biases.
+        w = training.utils.gaussian_weights_variable([num_nodes, output_size])
+        b = tf.Variable(tf.zeros([output_size]))
+
+        # Definition of the cell computation.
+        def lstm_cell(i, o, state):
+            """Create a LSTM cell. See e.g.: http://arxiv.org/pdf/1402.1128v1.pdf
+            Note that in this formulation, we omit the various connections between the
+            previous state and the gates."""
+            mult = tf.matmul(i, x_mat) + tf.matmul(o, o_mat) + b_vec
+
+            input_gate = tf.sigmoid(mult[:, 0:num_nodes])
+            forget_gate = tf.sigmoid(mult[:, num_nodes:2*num_nodes])
+            state = forget_gate * state + input_gate * tf.tanh(mult[:, 2*num_nodes:3*num_nodes])
+            output_gate = tf.sigmoid(mult[:, 3*num_nodes:4*num_nodes])
+            return output_gate * tf.tanh(state), state
+
+        # Input data.
+        before_embedding_size = input_size
+        if embedding is not None:
+            before_embedding_size = embedding.shape[0]
+
+        train_data = list()
+        for _ in range(num_unrollings + 1):
+            train_data.append(
+                tf.placeholder(tf.float32, shape=[batch_size, before_embedding_size]))
+        train_inputs = train_data[:num_unrollings]
+        train_labels = train_data[1:]  # labels are inputs shifted by one time step.
+
+        # Unrolled LSTM loop.
+        outputs = list()
+        output = saved_output
+        state = saved_state
+        for i in train_inputs:
+            if embedding is not None:
+                # Converting the input to the embedding
+                indices = tf.argmax(i, 1)
+                i = tf.nn.embedding_lookup(embedding, indices)
+            output, state = lstm_cell(i, output, state)
+            outputs.append(output)
+
+        # State saving across unrollings.
+        with tf.control_dependencies([saved_output.assign(output),
+                                      saved_state.assign(state)]):
+            # Classifier.
+            logits = tf.nn.xw_plus_b(tf.concat(0, outputs), w, b)
+            loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+                logits, tf.concat(0, train_labels)))
+
+        # Optimizer.
+        global_step = tf.Variable(0)
+
+        if exp_decay is not None:
+            learning_rate = tf.train.exponential_decay(
+                learning_rate, global_step,
+                exp_decay['decay_steps'], exp_decay['decay_rate'], exp_decay['staircase'])
+
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+        # Clipping to avoid exploding gradient.
+        gradients, v = zip(*optimizer.compute_gradients(loss))
+        gradients, _ = tf.clip_by_global_norm(gradients, gradient_max_value)
+        optimizer = optimizer.apply_gradients(zip(gradients, v), global_step=global_step)
+
+        # Predictions.
+        train_prediction = tf.nn.softmax(logits)
+
+        # Sampling and validation eval: batch 1, no unrolling.
+        sample_input_ph = tf.placeholder(tf.float32, shape=[1, before_embedding_size])
+        saved_sample_output = tf.Variable(tf.zeros([1, num_nodes]))
+        saved_sample_state = tf.Variable(tf.zeros([1, num_nodes]))
+        reset_sample_state = tf.group(
+            saved_sample_output.assign(tf.zeros([1, num_nodes])),
+            saved_sample_state.assign(tf.zeros([1, num_nodes])))
+
+        sample_input = sample_input_ph
+        if embedding is not None:
+            indices = tf.argmax(sample_input_ph, 1)
+            sample_input = tf.nn.embedding_lookup(embedding, indices)
+
+        sample_output, sample_state = lstm_cell(
+            sample_input, saved_sample_output, saved_sample_state)
+        with tf.control_dependencies([saved_sample_output.assign(sample_output),
+                                      saved_sample_state.assign(sample_state)]):
+            sample_prediction = tf.nn.softmax(tf.nn.xw_plus_b(sample_output, w, b))
+
+    tf_graph = {
+        'graph': graph,
+        'data_ph': train_data,
+        'sample_ph': sample_input_ph }
+    tf_predictions = [train_prediction, sample_prediction]
+
+    return tf_graph, optimizer, loss, tf_predictions, reset_sample_state
