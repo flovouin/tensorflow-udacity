36510a67e8dc76cc6e9a23d02458660206738a65
Flo <florent.vouin@gmail.com>
2016-02-10 21:03:22 +0100

    Comment training utilities.

--- src/training/utils.py
+++ src/training/utils.py
@@ -1,39 +1,49 @@
-import numpy as np
+# Provides utility functions when training neural networks.
+# Derived from Tensorflow's Udacity tutorial.
+#
+# Flo Vouin - 2016
+
 from matplotlib import pylab
+import numpy as np
 import random
 from sklearn.manifold import TSNE
 import tensorflow as tf
 
 def gaussian_weights_variable(shape):
+    """Initialises a weight variable with a truncated Gaussian distribution."""
     # The std is computed such that the variance of the sums
     # is 1/3, in order not to saturate the non-linearity too much.
     std = 1.0 / np.sqrt(3.0 * shape[0])
     return tf.Variable(tf.truncated_normal(shape, 0.0, std))
 
 def uniform_weights_variable(shape):
+    """Initialises a weight variable with a uniform distribution."""
     max_abs = 1.0 / np.sqrt(shape[0])
     return tf.Variable(tf.random_uniform(shape, -max_abs, max_abs))
 
 def gaussian_kernels_variable(shape):
+    """Initialises a variable containing kernels for a CNN
+    with a truncated Gaussian distribution."""
     # [height, width, depth, num_kernels]
     num_inputs = np.prod(shape[:3])
     std = 1.0 / np.sqrt(3.0 * num_inputs)
     return tf.Variable(tf.truncated_normal(shape, 0.0, std))
 
 def uniform_kernels_variable(shape):
+    """Initialises a variable containing kernels for a CNN
+    with a uniform distribution."""
     num_inputs = np.prod(shape[:3])
     max_abs = 1.0 / np.sqrt(num_inputs)
     return tf.Variable(tf.random_uniform(shape, -max_abs, max_abs))
 
 def logprob(predictions, labels):
-    """Log-probability of the true labels in a predicted batch."""
+    """Returns the log-probability of the true labels in a predicted batch."""
     predictions[predictions < 1e-10] = 1e-10
     return np.sum(np.multiply(labels, -np.log(predictions))) / labels.shape[0]
 
 def sample_distribution(distribution):
-    """Sample one element from a distribution assumed to be an array of normalized
-    probabilities.
-    """
+    """Samples one element from a distribution assumed to be an array of normalized
+    probabilities."""
     r = random.uniform(0, 1)
     s = 0
     for i in range(len(distribution)):
@@ -43,19 +53,19 @@ def sample_distribution(distribution):
     return len(distribution) - 1
 
 def sample(prediction):
-    """Turn a (column) prediction into 1-hot encoded samples."""
+    """Turns a (column) prediction into 1-hot encoded samples."""
     p = np.zeros(shape=[1, prediction[0].shape[0]], dtype=np.float)
     p[0, sample_distribution(prediction[0])] = 1.0
     return p
 
 def random_distribution(alphabet_size):
-    """Generate a random column of probabilities."""
+    """Generates a random column of probabilities."""
     b = np.random.uniform(0.0, 1.0, size=[1, alphabet_size])
     return b/np.sum(b, 1)[:,None]
 
-# Applies non-linear dimensionalty reduction using tSNE and plots
-# the words.
 def plot_embedding(embeddings, labels):
+    """Applies non-linear dimensionalty reduction using tSNE and plots
+    the words."""
     tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
     two_d_embeddings = tsne.fit_transform(embeddings)
 
