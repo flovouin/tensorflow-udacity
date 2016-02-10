883eced7a91f352a9bc34b03f9dda745aeca35a6
Flo <florent.vouin@gmail.com>
2016-02-10 21:05:21 +0100

    Clean up dataset utils and sixth assignment.

--- src/6_lstm.py
+++ src/6_lstm.py
@@ -1,12 +1,16 @@
 #! /usr/bin/env python3
+#
+# Solutions to the sixth assignment of Tensorflow's Udacity tutorial.
+#
+# Flo Vouin - 2016
 
 import collections
+from matplotlib import pylab
 import numpy as np
 import random
+from sklearn.manifold import TSNE
 import string
 import tensorflow as tf
-from matplotlib import pylab
-from sklearn.manifold import TSNE
 
 from dataset import text8, utils
 import training.batch
@@ -16,35 +20,31 @@ import training.utils
 
 data_folder = '../data'
 vocabulary_size = 50000
-alphabet_size = len(string.ascii_lowercase) + 1 # [a-z] + ' '
 valid_size = 1000
 
+# When training the character model.
 batch_size = 64
 num_unrollings = 10
 learning_rate = 10.0
 gradient_max_value = 1.25
-num_steps = 10001
+num_steps = 50001
 summary_frequency = 100
 exp_decay = {
     'decay_steps': 2500,
     'decay_rate': 0.5,
     'staircase': True
 }
-
 num_nodes = 64
 
+# Parameters when learning the embedding for bigrams.
 embedding_lr = 1.0
-embedding_size = 15 # Dimension of the embedding vector.
+embedding_size = 15
 dropout_prob = 0.2
 skip_batch_size = 128
-skip_window = 1 # How many words to consider left and right.
-num_skips = 2 # How many times to reuse an input to generate a label.
-# valid_size = 16 # Random set of words to evaluate similarity on.
-# valid_window = 100 # Only pick dev samples in the head of the distribution.
-# valid_examples = np.array(random.sample(range(valid_window), valid_size))
-num_sampled = 64 # Number of negative examples to sample.
+skip_window = 1
+num_skips = 2
+num_sampled = 64
 num_steps_embedding = 100001
-# num_points = 400
 
 # Preparing or loading the dataset.
 text, _, _, _, _ = text8.prepare_dataset(vocabulary_size, data_folder)
@@ -53,24 +53,25 @@ valid_text = text[:valid_size]
 train_text = text[valid_size:]
 train_size = len(train_text)
 
-# train_batches = training.batch.BatchGenerator(train_text, batch_size, num_unrollings, alphabet_size)
-# valid_batches = training.batch.BatchGenerator(valid_text, 1, 1, alphabet_size)
-#
-# # Model on single characters.
-# tf_graph, optimizer, loss, tf_predictions, reset_sample_state = \
-#     training.models.lstm_model(alphabet_size, alphabet_size, None, num_nodes,
-#         num_unrollings, batch_size, learning_rate, exp_decay, gradient_max_value)
-#
-# # Running.
-# training.graph_optimisation.run_lstm(tf_graph, optimizer, loss,
-#     reset_sample_state, tf_predictions, train_batches, valid_batches,
-#     valid_size, num_steps, utils.characters, summary_frequency)
+# Batches of data to train the character model.
+train_batches = training.batch.BatchGenerator(train_text, batch_size, num_unrollings, utils.alphabet_size)
+valid_batches = training.batch.BatchGenerator(valid_text, 1, 1, utils.alphabet_size)
+
+# Model on single characters.
+tf_graph, optimizer, loss, tf_predictions, reset_sample_state = \
+    training.models.lstm_model(utils.alphabet_size, utils.alphabet_size, None, num_nodes,
+        num_unrollings, batch_size, learning_rate, exp_decay, gradient_max_value)
+
+# Running optimisation of the model.
+training.graph_optimisation.run_lstm(tf_graph, optimizer, loss,
+    reset_sample_state, tf_predictions, train_batches, valid_batches,
+    valid_size, num_steps, utils.probs2chars, summary_frequency)
 
 # Model on bigrams.
 # Learning an embedding for bigrams.
-bialphabet_size = alphabet_size ** 2
-train_bitext = utils.char2bigrams(train_text)
-valid_bitext = utils.char2bigrams(valid_text)
+bialphabet_size = utils.alphabet_size ** 2
+train_bitext = utils.chars2bigrams(train_text)
+valid_bitext = utils.chars2bigrams(valid_text)
 
 skipgram_batches = training.batch.SkipgramBatchGenerator(train_bitext, skip_batch_size,
     num_skips, skip_window)
@@ -97,4 +98,4 @@ tf_graph, optimizer, loss, tf_predictions, reset_sample_state = \
 # Running.
 training.graph_optimisation.run_lstm(tf_graph, optimizer, loss,
     reset_sample_state, tf_predictions, train_bibatches, valid_bibatches,
-    valid_size, num_steps, utils.biprobstochar, summary_frequency)
+    valid_size, num_steps, utils.biprobs2chars, summary_frequency)
--- src/dataset/utils.py
+++ src/dataset/utils.py
@@ -1,3 +1,8 @@
+# Provides general utilities when preprocessing datasets.
+# Derived from Tensorflow's Udacity tutorial.
+#
+# Flo Vouin - 2016
+
 import numpy as np
 import os
 from six.moves.urllib.request import urlretrieve
@@ -16,27 +21,33 @@ def maybe_download(url, filepath, expected_bytes):
     return filepath
 
 def randomize(dataset, labels):
+    """Shuffles a dataset and the corresponding labels."""
     permutation = np.random.permutation(labels.shape[0])
     shuffled_dataset = dataset[permutation,:,:]
     shuffled_labels = labels[permutation]
     return shuffled_dataset, shuffled_labels
 
 def flatten_batch(data):
+    """Flattens a batch of multidimensional data and returns a 2D array."""
     return data.reshape((len(data), -1))
 
 def idx_to_onehot(idx, num_classes):
+    """Converts class indices to the one-hot representation."""
     onehot = (np.arange(num_classes) == idx[:,None]).astype(np.float32)
     return onehot
 
 def accuracy(predictions, labels):
-      return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
-          / predictions.shape[0])
+    """Compares probability distributions with one-hot encoded labels and
+    returns the corresponding accuracy as a percentage."""
+    return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+        / predictions.shape[0])
 
 # Character conversion.
 first_letter = ord(string.ascii_lowercase[0])
 alphabet_size = len(string.ascii_lowercase) + 1 # [a-z] + ' '
 
 def char2id(char):
+    """Converts a single character to an index."""
     if char in string.ascii_lowercase:
         return ord(char) - first_letter + 1
     elif char == ' ':
@@ -46,36 +57,41 @@ def char2id(char):
         return 0
 
 def id2char(dictid):
-    if dictid > 0:
+    """Converts a single index back to a character."""
+    if dictid > 0 and dictid < alphabet_size:
         return chr(dictid + first_letter - 1)
-    else:
+    elif dictid == 0:
         return ' '
+    else:
+        raise Exception('Unexpected index.')
 
-def characters(probabilities):
-    """Turn a 1-hot encoding or a probability distribution over the possible
-    characters back into its (mostl likely) character representation."""
+def probs2chars(probabilities):
+    """Turns a one-hot encoding or a probability distribution over the possible
+    characters back into its (most likely) character representation."""
     return [id2char(c) for c in np.argmax(probabilities, 1)]
 
-def batches2string(batches):
-    """Convert a sequence of batches back into their (most likely) string
+def batches2strings(batches):
+    """Converts a sequence of batches back into their (most likely) string
     representation."""
     s = [''] * batches[0].shape[0]
     for b in batches:
         s = [''.join(x) for x in zip(s, characters(b))]
     return s
 
-def ids(chars):
+def chars2ids(chars):
     """Converts characters to IDs."""
     return [char2id(c) for c in chars]
 
-def char2bigrams(text):
+def chars2bigrams(text):
+    """Converts a sequence of characters to bigram indices."""
     bitext_length = len(text) // 2
-    bitext = np.empty([bitext_length], dtype=int)
+    bitext = np.empty([bitext_length], dtype=np.int32)
     for i in range(bitext_length):
         bitext[i] = text[2*i] * alphabet_size + text[2*i + 1]
     return bitext
 
-def bigrams2char(bigrams):
+def bigrams2chars(bigrams):
+    """Converts bigram indices to a list of 2-character strings."""
     chars = []
     for b in range(len(bigrams)):
         chr1 = bigrams[b] // alphabet_size
@@ -83,13 +99,35 @@ def bigrams2char(bigrams):
         chars.append(''.join([id2char(chr1), id2char(chr2)]))
     return chars
 
-def biprobstochar(probabilities):
-    return bigrams2char(np.argmax(probabilities, 1))
+def biprobs2chars(probabilities):
+    """Converts probability distributions over bigrams to a list
+    of 2-character strings."""
+    return bigrams2chars(np.argmax(probabilities, 1))
 
 def bibatches2string(bibatches):
-    """Convert a sequence of batches back into their (most likely) string
-    representation."""
+    """Converts a sequence of bigram probability batches back into
+    their (most likely) string representation."""
     s = [''] * bibatches[0].shape[0]
     for b in bibatches:
         s = [''.join(x) for x in zip(s, biprobstochar(b))]
     return s
+
+# For sequence to sequence model.
+PAD_ID = 0
+GO_ID  = 1
+EOS_ID = 2
+
+def seq2seq_char2id(char):
+    if char in bytearray(string.ascii_lowercase, 'utf-8'):
+        return char - first_letter + 3
+    else:
+        print('Unexpected character:', char)
+        return 0
+
+def seq2seq_id2char(dictid):
+    if dictid > 2:
+        return chr(dictid + first_letter - 3)
+    elif dictid == EOS_ID:
+        return '.'
+    else:
+        return ' '
